ea84e1e30f7a60df451daf9161235dcfc5219447
Josh Palin <joshpalin@hotmail.com>
2017-06-07 23:27:52 +0000

    Added contact form view, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'    
+    resources :contacts
+    
     get 'contact', to: 'pages#contact'
     get 'testimonials', to: 'pages#testimonials'
     get 'pricing', to: 'pages#pricing'
