9566e2f383ed4fe3e89bffc2dc8a8dd9e4b6f8b0
Josh Palin <joshpalin@hotmail.com>
2017-06-07 23:43:54 +0000

    added form fields

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'    
     resources :contacts
-    
-    get 'contact', to: 'pages#contact'
-    get 'testimonials', to: 'pages#testimonials'
-    get 'pricing', to: 'pages#pricing'
 end
